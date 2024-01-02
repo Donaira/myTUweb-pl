db2986ff6aac91624e60ee54842b0a49f00651ee
MarcinSufa <sufa.marcin@gmail.com>
2024-01-02 16:39:25 +0100

    New version: added slovenian language

---
+++ _nuxt/9d85d49.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[22],{1274:function(n,t,e){"use strict";e.r(t);var o={asyncData:function(n){return(0,n.redirect)("/")}},c=e(14),component=Object(c.a)(o,void 0,void 0,!1,null,null,null);t.default=component.exports}}]);
